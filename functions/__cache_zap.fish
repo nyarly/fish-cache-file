ff7490e7dd8a36ce770a8eb96ab5cdb0454e8a8d
Judson <nyarly@gmail.com>
2016-05-19 14:14:46 -0700

    Bugfix for cache_zap

--- functions/__cache_zap.fish
+++ functions/__cache_zap.fish
@@ -1,5 +1,9 @@
 function __cache_zap --description 'Deletes cache files for the current directory under <ns>' --argument ns anchor extra_find
-	for f in (eval find (__cache_dir $ns) -name (__cache_key $anchor) $argv[3..-1] -print)
+	set -l constraints ""
+  if [ (count $argv) -gt 2 ]
+    set contraints $argv[3..-1]
+  end
+	for f in (eval find (__cache_dir $ns) -name (__cache_key $anchor) $contraints -print)
     rm $f
   end
 end
